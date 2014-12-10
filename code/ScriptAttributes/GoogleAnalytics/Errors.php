5aaafef929859dcf60bbffd1416e684f880d56c7
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2014-12-10 16:06:10 +1100

    FIX: Error code should work properly now

--- code/ScriptAttributes/GoogleAnalytics/Errors.php
+++ code/ScriptAttributes/GoogleAnalytics/Errors.php
@@ -13,8 +13,14 @@ use Milkyway\SS\ExternalAnalytics\ScriptAttributes\Contract;
 
 class Errors implements Contract {
 	public function output($controller, $params = [], \Milkyway\SS\ExternalAnalytics\Config\Contract $config = null, $prefix = '') {
-		if(($controller instanceof \ErrorPage_Controller) || (($response = $controller->Response) && ($response instanceof \SS_HTTPResponse) && $response->isError())) {
-			$errorCode = ($controller instanceof \ErrorPage_Controller) ? $controller->ErrorCode : isset($response) ? $response->getStatusCode() : 403;
+		$errorCode = '';
+
+		if($controller instanceof \ErrorPage_Controller)
+			$errorCode = $controller->ErrorCode;
+		elseif(($response = $controller->Response) && ($response instanceof \SS_HTTPResponse) && $response->isError())
+			$errorCode = $response->getStatusCode();
+
+		if($errorCode) {
 			$settings = '{ exDescription: \'' . _t('ErrorPage.' . $errorCode, $errorCode) . '\'}';
 			return '__' . $prefix . "('send', 'exception', {$settings});\n";
 		}
